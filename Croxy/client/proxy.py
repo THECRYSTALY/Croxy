6cf29709a88a01d48949c3f3dd64ffd668db8a1e
THECRYSTALY <crystaly.channel123@gmail.com>
2023-11-26 14:00:05 +0200

    added comments minor changes

--- Croxy/client/proxy.py
+++ Croxy/client/proxy.py
@@ -1,11 +1,20 @@
 import requests
 
 
-class Proxy:
-
+class HTTProxy:
+    """
+    class for HTTP prxoy.
+    """
     TEST_URL = 'http://httpbin.org/ip'
 
-    def __init__(self, address, port, session=False, proxy=True, auto_test=True):
+    def __init__(self, address: str, port: int = 80, session: bool = False, proxy: bool = True, auto_test: bool = True):
+        """
+        address: the address of the proxy.
+        port: port of the proxy, default is 80.
+        session: set it to True if you want to save token and cookies.
+        proxy: set it to False if you dont want a proxy to be used.
+        auto_test: automatically tests the proxy, set it to False if you dont want that.
+        """
         self.address = address
         self.port = port
         self.proxy = proxy
@@ -15,10 +24,6 @@ class Proxy:
     def test(self):
         """
         Tests the proxy using TEST_URL, used to set the proxy valid or invalid.
-
-        :return: Boolean.
-
-        todo add support for other protocols.
         """
         proxy = {
             "http": f"http://{self.address}:{self.port}",
@@ -35,9 +40,8 @@ class Proxy:
         """
         Sends a post request using the proxy.
 
-        :param args: Same args that requests.post take.
-        :param kwargs: same kwargs that requests.post take.
-        :return: Tuple
+        args: Same args that requests.post take.
+        kwargs: same kwargs that requests.post take.
         """
         proxy = {
             "http": f"{self.address}:{self.port}",
@@ -55,9 +59,8 @@ class Proxy:
         """
         Sends a get request using the proxy.
 
-        :param args: Same args that requests.get take.
-        :param kwargs: same kwargs that requests.get take.
-        :return: Tuple
+        args: Same args that requests.get take.
+        kwargs: same kwargs that requests.get take.
         """
         proxy = {
             "http": f"{self.address}:{self.port}",
@@ -75,9 +78,8 @@ class Proxy:
         """
         Sends a put request using the proxy.
 
-        :param args: Same args that requests.put take.
-        :param kwargs: same kwargs that requests.put take.
-        :return: Tuple
+        args: Same args that requests.put take.
+        kwargs: same kwargs that requests.put take.
         """
         proxy = {
             "http": f"{self.address}:{self.port}",
@@ -95,9 +97,8 @@ class Proxy:
         """
         Sends a delete request using the proxy.
 
-        :param args: Same args that requests.delete take.
-        :param kwargs: same kwargs that requests.delete take.
-        :return: Tuple
+        args: Same args that requests.delete take.
+        kwargs: same kwargs that requests.delete take.
         """
         proxy = {
             "http": f"{self.address}:{self.port}",
@@ -115,9 +116,8 @@ class Proxy:
         """
         Sends a get request using the proxy.
 
-        :param args: Same args that requests.patch take.
-        :param kwargs: same kwargs that requests.patch take.
-        :return: Tuple
+        args: Same args that requests.patch take.
+        kwargs: same kwargs that requests.patch take.
         """
         proxy = {
             "http": f"{self.address}:{self.port}",
@@ -135,9 +135,8 @@ class Proxy:
         """
         Sends a head request using the proxy.
 
-        :param args: Same args that requests.head take.
-        :param kwargs: same kwargs that requests.head take.
-        :return: Tuple
+        args: Same args that requests.head take.
+        kwargs: same kwargs that requests.head take.
         """
         proxy = {
             "http": f"{self.address}:{self.port}",
@@ -155,9 +154,8 @@ class Proxy:
         """
         Sends a options request using the proxy.
 
-        :param args: Same args that requests.options take.
-        :param kwargs: same kwargs that requests.options take.
-        :return: Tuple
+        args: Same args that requests.options take.
+        kwargs: same kwargs that requests.options take.
         """
         proxy = {
             "http": f"{self.address}:{self.port}",
